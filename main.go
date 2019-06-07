1c3f3821b8004af74cc5b4672d4e5a64f4df5852
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2019-06-07 14:09:17 +0600

    backslash

--- main.go
+++ main.go
@@ -9,17 +9,55 @@ import (
 )
 
 func unPackStr(line string) {
-	var arr, value string
-	for _, item := range line {
 
-		if unicode.IsDigit(item) {
-			arr += strings.TrimSpace(string(item))
-		} else {
-			arr += " " + string(item) + " "
+	var arr, value string
+	r := []rune(line)
+	r = append(r, ' ')
+
+	for i := 0; i <= len(r)-1; i++ {
+
+		// - 12345
+		if unicode.IsDigit(r[i]) {
+
+			if i != 0 {
+				// - qwe\\5
+				// если i == digit и перед ней есть \\, то i записываем как digit
+				if r[i-1] == 92 && r[i-2] == 92 {
+					arr += string(r[i])
+
+					// - qwe\45
+					//	иначе если i == digit и перед ним \ и после него digit, то записываем i как string
+				} else if r[i-1] == 92 && unicode.IsDigit(r[i+1]) {
+					arr += "_" + string(r[i]) + "_"
+
+					// - qwe\4\5
+					// иначе если i == digit и перед ним \ и после него не digit или конец строки, то записываем i как string
+				} else if r[i-1] == 92 && !unicode.IsDigit(r[i+1]) {
+					arr += "_" + string(r[i]) + "_"
+
+					// в остальных случаях 	i записываем как digit
+				} else {
+					arr += string(r[i])
+				}
+
+				// если i = 0 то i записываем как digit
+			} else {
+				arr += string(r[i])
+			}
+		}
+
+		// 	- qwerty
+		if unicode.IsLetter(r[i]) {
+			arr += "_" + string(r[i]) + "_"
+		}
+
+		// 	- \\
+		if r[i] == 92 && r[i-1] == 92 {
+			arr += "_" + string(r[i]) + "_"
 		}
 	}
 
-	l := strings.Split(arr, " ")
+	l := strings.Split(arr, "_")
 
 	for i := 2; i <= len(l); i = i + 2 {
 		number, _ := strconv.Atoi(l[i])
@@ -31,14 +69,23 @@ func unPackStr(line string) {
 	}
 
 	if value == "" {
-		fmt.Println("Incorrect String")
+		fmt.Println("Incorrect string")
 	} else {
 		fmt.Println(value)
 	}
 }
 
 func main() {
+
+	// "a4bc2d5e" => "aaaabccddddde"
+	// "abcd" => "abcd"
+	// "45"` => "" (некорректная строка)
+	// "qwe\4\5" => "qwe45" (*)
+	// "qwe\45" => "qwe44444" (*)
+	// "qwe\\5" => "qwe\\\\\" (*)
+
 	var text string
+
 	for text != "-" { // break the loop if text == "-"
 		fmt.Print("Enter your string: ")
 		fmt.Fscan(os.Stdin, &text)
